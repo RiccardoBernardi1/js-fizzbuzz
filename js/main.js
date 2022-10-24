4b05ab4aebc2239e87a4c3131679fe9669b110a3
RiccardoBernardi1 <rikyb3004@gmail.com>
2022-10-24 14:55:43 +0200

    Crea ciclo for per generare i numeri da 1 a 100

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// 1. Creare un ciclo for che generi i numeri da 1 a 100
+for (let i = 1; i<=100;i++){
+    console.log(i)
+}
+// 2. Trovare tutti i numeri multipli di 3,5 e 15 e sostiturli con fizz, buzz e fizzbuzz
+// 3. Creare un elemento in html per ogni numero e inserirci il numero o la stringa corrispondente
